2779bda0d8cd3c14d4f2b58892e62866783f8c97
Charles Xavier <charlesxsaldanha@hotmail.com>
2021-12-21 21:21:21 -0300

    requisitos 8 e 9. Ajustes no JS, comentando.

--- script.js
+++ script.js
@@ -1,22 +1,10 @@
+// Quadro de cores
+const colorPallete = document.querySelector('#color-palette');
 const primeiraCor = document.getElementById('cor1');
 const segundaCor = document.getElementById('cor2');
 const terceiraCor = document.getElementById('cor3');
 const quartaCor = document.getElementById('cor4');
 const seletorRgb = document.getElementById('color2');
-const inputPixelBoard = document.getElementById('board-size');
-const quadro = inputPixelBoard.value;
-
-for (let i = 0; i < quadro; i += 1) {
-  const horizontal = document.createElement('div');
-  horizontal.classList.add('horizontal');
-  document.getElementById('pixel-board').appendChild(horizontal);
-
-  for (let index = 0; index < quadro; index += 1) {
-    const vertical = document.createElement('div');
-    vertical.classList.add('pixel');
-    horizontal.appendChild(vertical);
-  }
-}
 
 function colors() {
   primeiraCor.style.backgroundColor = 'Black';
@@ -26,13 +14,53 @@ function colors() {
 }
 colors();
 
+// Criar o quadro de pixels
+const boardSize = document.querySelector('#board-size');
+const quadro = boardSize.value;
+
+function generateBoard() {
+  for (let i = 0; i < quadro; i += 1) {
+    const horizontal = document.createElement('div');
+    horizontal.classList.add('horizontal');
+    document.getElementById('pixel-board').appendChild(horizontal);
+
+    for (let index = 0; index < quadro; index += 1) {
+      const vertical = document.createElement('div');
+      vertical.classList.add('pixel');
+      horizontal.appendChild(vertical);
+    }
+  }
+}
+generateBoard();
+
+// Selecionar a cor
+
 function pickColor(event) {
-  const dfault = document.querySelector('.selected');
-  dfault.classList.remove('selected');
+  const defaultColor = document.querySelector('.selected');
+  defaultColor.classList.remove('selected');
   event.target.classList.add('selected');
 }
-primeiraCor.addEventListener('click', pickColor);
-segundaCor.addEventListener('click', pickColor);
-terceiraCor.addEventListener('click', pickColor);
-quartaCor.addEventListener('click', pickColor);
+colorPallete.addEventListener('click', pickColor);
 seletorRgb.addEventListener('click', pickColor);
+
+// Pintar o quadro
+const pixelEmpty = document.getElementsByClassName('pixel');
+
+function paintedPixel(event) {
+  event.target.style.backgroundColor = document.querySelector('.selected').style.backgroundColor;
+}
+
+for (pixels of pixelEmpty) {
+  pixels.addEventListener('click', paintedPixel);
+}
+
+// Botão de limpar o quadro
+
+const resetButton = document.querySelector('#clear-board');
+const pixelClear = document.querySelectorAll('.pixel');
+function reset() {
+  for (let index = 0; index < pixelClear.length; index += 1) {
+    pixelClear[index].style.backgroundColor = 'white';
+  }
+}
+resetButton.addEventListener('click', reset);
